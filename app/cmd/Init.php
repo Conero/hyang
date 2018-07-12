c4a3471fc9f8f92699024438302e211c7f97160c
Joshua Conero <brximl@163.com>
2018-07-12 23:08:55 +0800

    TODO 编写说明

---
+++ app/cmd/Init.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Auther: Joshua Conero
+ * Date: 2018/7/12 0012 16:42
+ * Email: brximl@163.com
+ * Name: 项目初始化
+ */
+
+namespace app\cmd;
+
+
+use hyang\surong\cmd\Controller;
+
+class Init extends Controller
+{
+    // 默认赋值
+    public function DefaultAction(){}
+}
--- index.php
+++ index.php
@@ -6,11 +6,13 @@
  * Name: ppm php 包管理工具
  */
 use \hyang\surong\cmd\Cmd;
+use \hyang\surong\cmd\Fmt;
 require_once __DIR__.'/vendor/ppm.php';
 
 
 Cmd::Init(__DIR__.'/');
 
+// 路由函数
 $RouterFn = function ($cmd, $act=null){
     $app = 'app\\cmd\\'.$cmd;
     if(class_exists($app)){
@@ -30,12 +32,22 @@ $RouterFn = function ($cmd, $act=null){
                 echo $action.'不存在! '. "\n";
             }
         }
-
+    }else{
+        return true;
     }
 };
 // 运行路由
 Cmd::router('[command]/[action]', $RouterFn);
 // 默认路由
 Cmd::router('[command]', $RouterFn);
+// 解析失败
+Cmd::router(Cmd::rUnfind, function ($cmd){
+    Fmt::line($cmd.' 命令不存在！！');
+});
+// 空解析方式
+Cmd::router(Cmd::rEmpty, function (){
+    Fmt::line('命令行参数： ');
+    Fmt::line('git 从开源的git仓库中获取数据！');
+});
 
 Cmd::Run($argv);
